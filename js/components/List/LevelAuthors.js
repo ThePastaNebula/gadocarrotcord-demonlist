21d95402369d4186a81dcda7434558da58e13b7c
ThePastaNebula <65950050+ThePastaNebula@users.noreply.github.com>
2025-03-25 01:57:30 +0000

    Add files via upload

---
+++ js/components/List/LevelAuthors.js
@@ -0,0 +1,40 @@
+export default {
+    props: {
+        author: {
+            type: String,
+            required: true,
+        },
+        creators: {
+            type: Array,
+            required: true,
+        },
+        verifier: {
+            type: String,
+            required: true,
+        },
+    },
+    template: `
+        <div class="level-authors">
+            <template v-if="selfVerified">
+                <div class="type-title-sm">Creator & Verifier</div>
+                <p class="type-body">
+                    <span>{{ author }}</span>
+                </p>
+            </template>
+            <div class="type-title-sm">Publisher</div>
+            <p class="type-body">
+                <span>{{ author }}</span>
+            </p>
+            <div class="type-title-sm">Verifier</div>
+            <p class="type-body">
+                <span>{{ verifier }}</span>
+            </p>
+        </div>
+    `,
+
+    computed: {
+        selfVerified() {
+            return this.author === this.verifier && this.creators.length === 0;
+        },
+    },
+};
